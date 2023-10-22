c65a40a72e5f1385651eba3eee7baebbde74e400
LuncanuAlexandru <127941060+LuncanuAlexandru@users.noreply.github.com>
2023-10-22 18:29:56 +0300

    tema nr.2

---
+++ Luncanu Alexandru-Cosmin grupa 3133/OpenTK_console_sample02/OpenTK_console_sample02.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+/**
+    Aplicația utilizează biblioteca OpenTK v2.0.0 (stable) oficială și OpenTK. GLControl v2.0.0
+    (unstable) neoficială. Aplicația fiind scrisă în modul consolă nu va utiliza controlul WinForms
+    oferit de OpenTK!
+    Tipul de ferestră utilizat: GAMEWINDOW. Se demmonstrează modul imediat de randare (vezi comentariul!)...
+**/
+namespace OpenTK_console_sample02
+{
+    class SimpleWindow3D : GameWindow
+    {
+        bool moveForward = false;
+        bool moveBackward = false;
+        bool moveLeft = false;
+        bool moveRight = false;
+        float objectX = 0.0f;
+        float objectZ = 0.0f;
+        float objectY = 0.0f;   
+
+        const float rotation_speed = 180.0f;
+        float angle;
+        bool showCube = true;
+        KeyboardState lastKeyPress;
+
+        // Constructor.
+        public SimpleWindow3D() : base(800, 600) {
+            VSync = VSyncMode.On;
+        }
+
+        /**Setare mediu OpenGL și încarcarea resurselor (dacă e necesar) - de exemplu culoarea de
+           fundal a ferestrei 3D.
+           Atenție! Acest cod se execută înainte de desenarea efectivă a scenei 3D. */
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
+
+            GL.ClearColor(Color.Blue);
+            //GL.Enable(EnableCap.DepthTest);
+        }
+
+        /**Inițierea afișării și setarea viewport-ului grafic. Metoda este invocată la redimensionarea
+           ferestrei. Va fi invocată o dată și imediat după metoda ONLOAD()!
+           Viewport-ul va fi dimensionat conform mărimii ferestrei active (cele 2 obiecte pot avea și mărimi 
+           diferite). */
+        protected override void OnResize(EventArgs e) {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+
+            double aspect_ratio = Width / (double)Height;
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+
+            
+        }
+
+        /** Secțiunea pentru "game logic"/"business logic". Tot ce se execută în această secțiune va fi randat
+            automat pe ecran în pasul următor - control utilizator, actualizarea poziției obiectelor, etc. */
+        protected override void OnUpdateFrame(FrameEventArgs e) {
+            base.OnUpdateFrame(e);
+
+            KeyboardState keyboard = OpenTK.Input.Keyboard.GetState();
+
+            // Control obiect folosind tastele W, A, S, D
+            moveForward = keyboard[Key.W];
+            moveBackward = keyboard[Key.S];
+            moveLeft = keyboard[Key.A];
+            moveRight = keyboard[Key.D];
+
+            // Actualizare poziție obiect
+            if (moveForward) objectY += 0.1f;
+            if (moveBackward) objectY -= 0.1f;
+            if (moveLeft) objectX -= 0.1f;
+            if (moveRight) objectX += 0.1f;
+        }
+
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            // Control obiect folosind mișcarea mouse-ului
+            objectX = e.X / (float)Width * 2 - 1; // Map X mouse position to OpenGL coordinate
+            objectZ = (Height - e.Y) / (float)Height * 2 - 1; // Map Y mouse position to OpenGL coordinate
+        }
+
+        /** Secțiunea pentru randarea scenei 3D. Controlată de modulul logic din metoda ONUPDATEFRAME().
+            Parametrul de intrare "e" conține informatii de timing pentru randare. */
+        protected override void OnRenderFrame(FrameEventArgs e) {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 lookat = Matrix4.LookAt(10,10, 20, objectX, objectY, objectZ, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+            // Restul codului pentru randarea obiectului (cub, triunghi etc.)
+            DrawCube();
+            SwapBuffers();
+            //Thread.Sleep(1);
+        }
+
+        private void DrawAxes_OLD() {
+            GL.Begin(PrimitiveType.Lines);
+
+            // X
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(20, 0, 0);
+
+            // Y
+            GL.Color3(Color.Blue);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 20, 0);
+
+            // Z
+            GL.Color3(Color.Yellow);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, 20);
+
+
+            GL.End();
+        }
+
+        // Utilizăm modul imediat!!!
+        private void DrawCube() {
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.Color3(Color.Silver);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+
+            GL.Color3(Color.Red);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+
+            GL.Color3(Color.Moccasin);
+
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.IndianRed);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+
+            GL.Color3(Color.PaleVioletRed);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.ForestGreen);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+
+            GL.End();
+        }
+
+        [STAThread]
+        static void Main(string[] args) {
+
+            /**Utilizarea cuvântului-cheie "using" va permite dealocarea memoriei o dată ce obiectul nu mai este
+               în uz (vezi metoda "Dispose()").
+               Metoda "Run()" specifică cerința noastră de a avea 30 de evenimente de tip UpdateFrame per secundă
+               și un număr nelimitat de evenimente de tip randare 3D per secundă (maximul suportat de subsistemul
+               grafic). Asta nu înseamnă că vor primi garantat respectivele valori!!!
+               Ideal ar fi ca după fiecare UpdateFrame să avem si un RenderFrame astfel încât toate obiectele generate
+               în scena 3D să fie actualizate fără pierderi (desincronizări între logica aplicației și imaginea randată
+               în final pe ecran). */
+            Console.WriteLine();
+            using (SimpleWindow3D example = new SimpleWindow3D()) {
+
+                // Verificați semnătura funcției în documentația inline oferită de IntelliSense!
+                example.Run(30.0, 0.0);
+            }
+        }
+    }
+
+}
